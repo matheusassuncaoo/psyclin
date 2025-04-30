1bc53e2f2751ac596e48488aa0bf6edddc05770b
matheusassuncaoo <matheus.assuncaomt18@gmail.com>
2025-04-30 15:03:17 -0400

    mega-update

---
+++ src/main/java/com/br/psyclin/PsyclinApplication.java
@@ -0,0 +1,13 @@
+package com.br.psyclin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsyclinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsyclinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/psyclin/PsyclinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.psyclin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsyclinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
