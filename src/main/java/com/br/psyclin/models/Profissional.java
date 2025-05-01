8a8df4e21138cb5e076b8a6af07d3fc2bea8322e
matheusassuncaoo <matheus.assuncaomt18@gmail.com>
2025-05-01 15:02:31 -0400

    v1.0

---
+++ src/main/java/com/br/psyclin/models/Profissional.java
@@ -0,0 +1,10 @@
+package com.br.psyclin.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profissional")
+public class Profissional {
+    public static final String TABLE_NAME = "Profissional";
+}
