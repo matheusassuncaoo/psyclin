99fbd72d52794425cf947d1e66044022b28b266d
matheusassuncaoo <matheus.assuncaomt18@gmail.com>
2025-05-09 17:04:29 -0400

    v1.2

--- src/main/java/com/br/psyclin/repositories/ProfissionalRepository.java
+++ src/main/java/com/br/psyclin/repositories/ProfissionalRepository.java
@@ -4,8 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.br.psyclin.models.Profissional;
 
+import java.util.List;
+
 @Repository
 public interface ProfissionalRepository extends JpaRepository<Profissional, Long> {
 
     Profissional findByCodProf(String codProf);
+
+    List<Profissional> findBySupProf(String supProf);
+
+    List<Profissional> findByTipoProf(Integer tipoProf);
+
+    List<Profissional> findByStatusProf(Integer statusProf);
 }
