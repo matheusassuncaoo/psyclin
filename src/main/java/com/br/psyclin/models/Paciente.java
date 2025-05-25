0abd71835bb4a1c4c0e47a6aaaafe4dbddbb0e69
matheusassuncaoo <matheus.assuncaomt18@gmail.com>
2025-05-25 17:18:24 -0400

    v1.2.5.2 - NotBlank para Strings

--- src/main/java/com/br/psyclin/models/Paciente.java
+++ src/main/java/com/br/psyclin/models/Paciente.java
@@ -9,7 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -33,32 +33,31 @@ public class Paciente {
 
     public static final String TABLE_NAME = "Paciente";
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "cpf_pac",nullable = false, length = 11)
     @NotNull(groups = CreatePaciente.class)
-    @NotEmpty(groups = CreatePaciente.class)
+    @NotBlank(groups = CreatePaciente.class)
     @Size(groups = CreatePaciente.class, min = 11, max = 11)
-    private String cpfPac; // CPF como chave primária
+    private String cpfPac;
 
     @Column(name = "cod_pac", length = 10)
     @NotNull
-    @NotEmpty
+    @NotBlank
     @Size(min = 10, max = 10)
     private String codPac;
 
     @Column(name = "nome_pac", length = 100)
     @NotNull
-    @NotEmpty
+    @NotBlank
     @Size(min = 50, max = 100)
     private String nomePac;
 
     @Column(name = "tel_pac", length = 11)
     @NotNull
-    @NotEmpty
+    @NotBlank
     @Size(min = 11, max = 11)
     private String telPac;
 
@@ -68,28 +67,24 @@ public class Paciente {
 
     @Column(name = "rg_pac", length = 12)
     @NotNull
-    @NotEmpty
+    @NotBlank
     @Size(min = 12, max = 12)
     private String rgPac;
 
     @Column(name = "est_rg_pac", length = 2)
     @NotNull
-    @NotEmpty
+    @NotBlank
     @Size(min = 2, max = 2)
     private String estRgPac;
 
     @Column(name = "nome_mae_pac", length = 100)
     @NotNull
-    @NotEmpty
+    @NotBlank
     @Size(min = 50, max = 100)
     private String nomeMaePac;
 
     @Column(name = "status_pac", length = 1)
     @NotNull
-    @NotEmpty
     @Size(min = 1, max = 1)
     private Integer statusPac; // 1: ativo, 2: inativo, 3: suspenso
-
-
-  
 }
